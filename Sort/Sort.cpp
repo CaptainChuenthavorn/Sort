2e408693fd49bae8584eea11b4019f086072e65e
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-10-16 22:56:52 +0700

    can separate word

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -4,46 +4,69 @@
 int main()
 {
 	int ary = 1,maxAry=0,countAry=0;
+	int i;
 	char str[100];
 	char* p, ** pSpace;
 	scanf("%[^\n]s", str);
 	p = str; 
-	printf("%s", str);
+	printf("str : %s\n", str);
 	while (*p != '\0') {
 		if (*p == ' ') { ary++; } //add number of space(ary) 
 		p++;
 	}
-
+	pSpace = (char**)malloc(ary * sizeof(int)); // reserve area
 	p = str; // reset p to str[0]
 
-	while (*p != '\0') {
+	while (*p != '\0') {	//find number word largest ary 
 		if (*p != ' ') { 
 
 			countAry++; 
-			if()
-			if (*p == ' ') {
-				countAry = 0;
+			if (countAry > maxAry) {
+				maxAry = countAry;
 			}
-		} //find number word largest ary 
+		} 
+		else if (*p == ' ') {
+			countAry = 0;
+		}
 		p++;
 	}
-
-	pSpace = (char**)malloc(ary * sizeof(int));
+	printf("\nmax : %d\n",maxAry);
+	p = str;
+	for(i=0;i<ary;i++){
+	pSpace[i] = (char*)malloc(maxAry * sizeof(int));
+	}
 	for (int j = 0;j < ary;j++) {
-		for (int i = 0;i < 3;i++) {
-			if (*p == ' ') {
+		for (int i = 0;i < maxAry;i++) {
+			/*if (*p == ' ') {
 				p++;  
 			}
-			while (*p != ' ') {
-				pSpace[j][i] = *p;
+			else if (*p != ' '||*p >= 'a' && *p<='z' && *p>= 'A' && *p<='Z') {
+				pSpace[j][i] = *p;	//add c to pSpace[j][i]
 				p++;
+			}*/
+			if ((*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z')) {
+				pSpace[j][i] = *p;	//add c to pSpace[j][i]
+				p++;
+			}
+			else if (*p == ' ' && j>0){
+				for(int k = 0;k<(maxAry-i+1);k++){
+					pSpace[j][i] = ' ';	//add c to pSpace[j][i]
+					i++;
+				}
+				p++;
+			}
+			else {
+				pSpace[j][i] = ' ';
 			}
 		}
 	}
+	printf("\npSpace\n");
 	for (int j = 0;j < ary;j++) {
-		for (int i = 0;i < ary;i++) {
-			printf("%c",pSpace[j][i]);
+		for (int i = 0;i < maxAry;i++) {
+			printf("[%d] [%d] : %c  ",j,i,pSpace[j][i]);
+			//printf(" %c ",pSpace[j][i]);
 		}
+		printf("\n");
 	}
 	//printf("\nspace : %d\n pScape : %s", ary, pSpace[1]);
 	return 0;
