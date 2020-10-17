74a1c273c2c95bccf83c8134fa0934f4d0c36466
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-10-17 15:21:46 +0700

    finish

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -4,17 +4,17 @@
 int main()
 {
 	int ary = 1,maxAry=0,countAry=0;
-	int i;
+	int i, space = 0;;
 	char str[100];
 	char* p, ** pSpace;
-	scanf("%[^\n]s", str);
+	scanf("%[^\n]s", str); //input sentence
 	p = str; 
-	printf("str : %s\n", str);
+	printf("Message : %s\n", str);
 	while (*p != '\0') {
 		if (*p == ' ') { ary++; } //add number of space(ary) 
 		p++;
 	}
-	pSpace = (char**)malloc(ary * sizeof(int)); // reserve area
+	pSpace = (char**)malloc(ary * sizeof(int)); // reserve area[]
 	p = str; // reset p to str[0]
 
 	while (*p != '\0') {	//find number word largest ary 
@@ -30,44 +30,45 @@ int main()
 		}
 		p++;
 	}
-	printf("\nmax : %d\n",maxAry);
+	printf("max character : %d\n", maxAry);
 	p = str;
-	for(i=0;i<ary;i++){
-	pSpace[i] = (char*)malloc(maxAry * sizeof(int));
-	}
-	for (int j = 0;j < ary;j++) {
+		for(i=0;i<ary;i++){
+			pSpace[i] = (char*)malloc(maxAry * sizeof(int));//reserve area[][]
+		}
+	for (int j = 0;j < ary;j++) {	//method : put *p in [][]
 		for (int i = 0;i < maxAry;i++) {
-			/*if (*p == ' ') {
-				p++;  
-			}
-			else if (*p != ' '||*p >= 'a' && *p<='z' && *p>= 'A' && *p<='Z') {
-				pSpace[j][i] = *p;	//add c to pSpace[j][i]
-				p++;
-			}*/
+			jumper:
 			if ((*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z')) {
-				pSpace[j][i] = *p;	//add c to pSpace[j][i]
+				pSpace[j][i] = *p;	//add char to pSpace[j][i]
 				p++;
 			}
-			else if (*p == ' ' && j>0){
-				for(int k = 0;k<(maxAry-i+1);k++){
-					pSpace[j][i] = ' ';	//add c to pSpace[j][i]
-					i++;
+			else if (*p == ' ') {	
+				if (i == (maxAry)|| i == 0) {
+					p++;
+					goto jumper;
 				}
-				p++;
+				else {
+					while (i < maxAry)
+					{ 
+					pSpace[j][i] = ' ';	
+					i++;
+					}
+					p++;
+				}
 			}
-			else {
+			else {	//at last word < maxAry
 				pSpace[j][i] = ' ';
 			}
 		}
 	}
-	printf("\npSpace\n");
-	for (int j = 0;j < ary;j++) {
+	printf("\nSeparate to array\n");
+	for (int j = 0;j < ary;j++) {	//print result
+		printf("word %d : ",j+1);
 		for (int i = 0;i < maxAry;i++) {
-			printf("[%d] [%d] : %c  ",j,i,pSpace[j][i]);
-			//printf(" %c ",pSpace[j][i]);
+			//printf("[%d][%d] : %c  ",j,i,pSpace[j][i]);
+			printf("%c", pSpace[j][i]);
 		}
 		printf("\n");
 	}
-	//printf("\nspace : %d\n pScape : %s", ary, pSpace[1]);
 	return 0;
 }
