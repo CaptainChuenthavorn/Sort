0502136fc5ab5e106d1397a8eece5fb41eafae10
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-10-17 15:30:01 +0700

    add function Max

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -1,6 +1,7 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+int Max(int , int, char* );
 int main()
 {
 	int ary = 1,maxAry=0,countAry=0;
@@ -61,7 +62,8 @@ int main()
 			}
 		}
 	}
-	printf("\nSeparate to array\n");
+	printf("\nSeparate to word by word\n");
+	
 	for (int j = 0;j < ary;j++) {	//print result
 		printf("word %d : ",j+1);
 		for (int i = 0;i < maxAry;i++) {
@@ -71,4 +73,20 @@ int main()
 		printf("\n");
 	}
 	return 0;
+}
+int Max(int maxAry,int countAry, char *p) {
+	while (*p != '\0') {	//find number word largest ary 
+		if (*p != ' ') {
+
+			countAry++;
+			if (countAry > maxAry) {
+				maxAry = countAry;
+			}
+		}
+		else if (*p == ' ') {
+			countAry = 0;
+		}
+		p++;
+	}
+	return maxAry;
 }
